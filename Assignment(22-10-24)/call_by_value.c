82d465653a73e7d64a1fcc8026fd17edc7a144f4
Kalyanhunu <kalyanhunu@gmail.com>
2024-10-24 09:14:10 +0530

    Create call_by_value.c

---
+++ Assignment(22-10-24)/call_by_value.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void swap(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("Swapped Values are as, a : %d and b : %d", a, b);
+}
+
+main()
+{
+    int x, y;
+    printf("Enter two numbers ( x and y ) : ");
+    scanf("%d%d", &x, &y);
+    swap(x, y);
+}
